121c2692499ebe6e14a7b8be64f5f00b42f220ae
Artem Denysiuk <artem2002gurt@gmail.com>
2023-09-06 17:58:54 +0300

    git init

---
+++ src/main/java/com/tutoring_calendar/TutoringCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.tutoring_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoring_calendar/TutoringCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoring_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
